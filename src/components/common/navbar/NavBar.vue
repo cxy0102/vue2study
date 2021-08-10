7d21901fb556805c6686b5ad9b3adb3e723200d9
wjz <279646436@qq.com>
2021-08-10 17:11:45 +0800

    feat: 添加导航栏

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,35 @@
+<!--
+ * @file: NavBar
+ * @Dsc: 描述
+ * @Author wjz
+ * @Date 2021/8/10 16:35
+-->
+<template>
+  <div class="nav-bar">
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot name="center"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style scoped>
+  .nav-bar{
+    display: flex;
+    height: 44px;
+    text-align: center;
+    box-shadow: 0 1px 1px rgba(100,100,100 ,.2);
+  }
+  .left, .right{
+    width: 60px;
+  }
+  .center{
+    flex: 1;
+  }
+
+</style>
