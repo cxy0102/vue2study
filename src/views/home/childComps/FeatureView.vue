e27b123bce17b87598bf3a9e774c1aa4676c77b8
wjz <279646436@qq.com>
2021-08-12 17:15:12 +0800

    feat: 实现首页图片导航(本周流行)功能

---
+++ src/views/home/childComps/FeatureView.vue
@@ -0,0 +1,25 @@
+<!--
+ * @file: featureView
+ * @Dsc: 描述
+ * @Author wjz
+ * @Date 2021/8/12 16:01
+-->
+<template>
+  <div class="feature">
+    <a herf="http://act.mogujie.com/zzlx67">
+      <img  src="@/assets/img/home/recommend_bg.jpg" alt="">
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "featureView"
+}
+</script>
+
+<style scoped>
+ .feature img{
+   width: 100%;
+ }
+</style>
